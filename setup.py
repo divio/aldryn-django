45d67ba4fb3c367c701f4403e0bd71000c0831a5
Stefan Foulis <stefan@foulis.ch>
2016-05-10 16:35:26 +0200

    upgrade Django to 1.8.11

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.8.10',
+        'Django==1.8.11',
 
         # setup utils
         'dj-database-url',
