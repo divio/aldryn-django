cc95af8373402a1dffb3bedb9257bf7f00397704
Stefan Foulis <stefan@foulis.ch>
2016-05-30 21:57:19 +0200

    redirect to admin by default
    so that first time visitors of a new site don't just get a 404 error

---
+++ aldryn_django/urls_redirect.py
@@ -0,0 +1,8 @@
+
+from django.conf.urls import url
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
+
+urlpatterns = [
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
+]
