96c0ca8b1873f82aaf0a00b979a9167a1f513e6f
Martin Koistinen <mkoistinen@gmail.com>
2016-06-27 22:17:58 -0400

    Use Django 1.8.13, bump to v1.8.13.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.8.11',
+        'Django==1.8.13',
 
         # setup utils
         'dj-database-url',
