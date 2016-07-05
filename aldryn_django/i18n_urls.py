31311d4b7cc21a680030ec41e748f0fd90657899
Stefan Foulis <stefan@foulis.ch>
2016-07-05 07:38:32 +0200

    geodjango support

--- aldryn_django/i18n_urls.py
+++ aldryn_django/i18n_urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf import settings
+
+if settings.ALDRYN_DJANGO_ENABLE_GIS:
+    from django.contrib.gis import admin
+else:
+    from django.contrib import admin
 
 
 admin.autodiscover()
