9853cbbc53d5fd47c3a276372045de92088293db
Paulo <commonzenpython@gmail.com>
2016-07-19 09:58:07 -0400

    Bumped version to 1.9.8.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.9.7',
+        'Django==1.9.8',
 
         # setup utils
         'dj-database-url',
