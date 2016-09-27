a2062fb63b02b02bdd532d5783c7e7722551ae0a
Paulo <commonzenpython@gmail.com>
2016-09-26 20:51:37 -0400

    Bumped version to 1.8.15.0 - django 1.8.15

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.8.14',
+        'Django==1.8.15',
 
         # setup utils
         'dj-database-url',
