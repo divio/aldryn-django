1bac7f0c4f3c16c3cae723d22c7500a8939a3db0
Paulo Alvarado <commonzenpython@gmail.com>
2016-11-01 10:49:52 -0400

    Bumped version to 1.8.16.0 - django 1.8.16 (#77)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.8.15',
+        'Django==1.8.16',
 
         # setup utils
         'dj-database-url',
