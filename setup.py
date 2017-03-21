55fc7cfef8a67945f1b2c1ca3e3279bfd856017b
Stefan Foulis <stefan@foulis.ch>
2017-03-21 17:20:30 +0100

    Release 1.9.12.0 - Django 1.9.12

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.9.11',
+        'Django==1.9.12',
 
         # setup utils
         'dj-database-url',
