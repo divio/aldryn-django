3cd287494186881163b3885cb6989126d718c6b1
Stefan Foulis <stefan@foulis.ch>
2017-03-21 17:23:26 +0100

    Release 1.10.6.0 - Django 1.10.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.10.5',
+        'Django==1.10.6',
 
         # setup utils
         'dj-database-url',
