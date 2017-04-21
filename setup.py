059a4fd152deb0611cd3a38f580562ee38f28aba
Stefan Foulis <stefan@foulis.ch>
2017-04-22 00:38:21 +0200

    Release 1.11.0.1 - Django 1.11

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.10.7',
+        'Django==1.11.0.1',
 
         # setup utils
         'dj-database-url',
