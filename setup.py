94423f316d522931a2e7aa43a6115eae8388284d
Paulo Alvarado <commonzenpython@gmail.com>
2017-09-06 07:55:39 -0400

    Release 1.11.5.1 - Django 1.11.5 (#93)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.11.3',
+        'Django==1.11.5',
 
         # setup utils
         'dj-database-url',
