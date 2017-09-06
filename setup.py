d1089d97df2ad12b3e53ab3cdb473a11efc32ead
Paulo Alvarado <commonzenpython@gmail.com>
2017-09-06 07:55:34 -0400

    Release 1.10.8.1 - Django 1.10.8 (#92)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.10.7',
+        'Django==1.10.8',
 
         # setup utils
         'dj-database-url',
