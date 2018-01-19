84d93a7131485c46fb9d5725d22e76f4e3202be0
Stefan Foulis <stefan@foulis.ch>
2018-01-19 16:39:18 +0100

    Release 1.11.9.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.11.5',
+        'Django==1.11.9',
 
         # setup utils
         'dj-database-url',
