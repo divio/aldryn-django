3a52b57f2a20788ad355b795b153062ffe1664dd
Paulo Alvarado <commonzenpython@gmail.com>
2018-03-07 11:30:44 -0500

    Release 1.9.13.5 - django 1.9.13.post1 (#107)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.9.13',
+        'Django==1.9.13.post1',
 
         # setup utils
         'dj-database-url',
