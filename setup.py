6fe4c88af2bba4b87a0782e7b748f92091c1c817
Filipe Waitman <filwaitman@gmail.com>
2018-08-01 16:18:59 +0200

    Release 1.11.15.1 - Django 1.11.15

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==1.11.11',
+        'Django==1.11.15',
 
         # setup utils
         'dj-database-url',
