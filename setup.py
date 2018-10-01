e27ae2d4403091945c79fb7cdc45e899b753e089
Filipe Waitman <filwaitman@gmail.com>
2018-10-01 11:11:14 -0300

    Release 2.1.2.1 - Django 2.1.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==2.0.8',
+        'Django==2.1.2',
 
         # setup utils
         'dj-database-url',
