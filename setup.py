b0c52368d590d8dfb48a3fc388b0526bb0e40ff1
Angelo Dini <angelo.dini@divio.ch>
2019-01-07 09:36:29 +0100

    Security release 2.0.10 (#128)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=(
         'aldryn-addons',
-        'Django==2.0.9',
+        'Django==2.0.10',
 
         # setup utils
         'dj-database-url',
