1d4c964812d6588c3f1bc8259b5de3f117159527
Angelo Dini <angelo.dini@divio.ch>
2019-01-25 10:18:30 +0100

    files cleanup

--- aldryn_django/models.py
+++ aldryn_django/models.py
@@ -1,5 +1,3 @@
-#######################
-# apply monkeypatches #
-#######################
+# -*- coding: utf-8 -*-
 from .monkeypatches import hide_secrets_in_debug_mode
 hide_secrets_in_debug_mode.patch()
--- aldryn_django/urls_redirect.py
+++ aldryn_django/urls_redirect.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
