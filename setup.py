a1c7dfd5cd012095023b926c51d7279a3f8abed7
Angelo Dini <angelo.dini@divio.ch>
2019-01-28 12:51:29 +0100

    removed uncompatible Django references

--- setup.py
+++ setup.py
@@ -60,8 +60,6 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Framework :: Django',
     'Framework :: Django :: 1.11',
-    'Framework :: Django :: 2.0',
-    'Framework :: Django :: 2.1',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
