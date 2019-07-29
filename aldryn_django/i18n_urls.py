9353f64a11b405f96d742d302f38ea5953d75056
Angelo Dini <angelo.dini@divio.ch>
2019-07-29 14:45:44 +0200

    Bumped version to 2.1.10.4 (#173)
    * * Serve static files from ``django.conf.urls.static`` during local development
    
    * running isort

--- aldryn_django/i18n_urls.py
+++ aldryn_django/i18n_urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import re_path
 
 
@@ -14,3 +15,7 @@ admin.autodiscover()
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
 ]
+
+# serve static files differently on local development
+if settings.IS_RUNNING_DEVSERVER and not settings.MEDIA_URL_IS_ON_OTHER_DOMAIN:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
