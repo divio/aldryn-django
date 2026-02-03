6e352b784dea14efccbbbc975d8a3d4a45f8cfed
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2026-02-03 15:21:32 +0100

    chore: bumped version to 4.2.28.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from aldryn_django import __version__
 
 REQUIREMENTS = [
     "aldryn-addons",
-    "Django==4.2.27",
+    "Django==4.2.28",
     # setup utils
     "dj-database-url",
     "dj-email-url",
